629290ad941178b2bc368b2ff0fe5ac144bfe10c
Faisal Alhassan <devfalhassan@gmail.com>
2023-05-23 05:56:48 +0100

    added middlewares functions

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello api');
+});
+
+module.exports = app;
